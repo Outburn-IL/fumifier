8892245a4d6aec2f12fdd560aa7250bbb4a064a1
mechanik-daniel <daniel@outburn.co.il>
2026-06-17 12:28:44 +0300

    test: update ilcore to v0.21.0

--- test/flash-parser-hardening.test.js
+++ test/flash-parser-hardening.test.js
@@ -92,7 +92,7 @@ describe('FLASH parser hardening regression', function() {
     this.timeout(720000);
 
     const fpe = await FhirPackageExplorer.create({
-      context: ['il.core.fhir.r4#0.17.0', 'fumifier.test.pkg#0.1.0'],
+      context: ['il.core.fhir.r4#0.21.0', 'fumifier.test.pkg#0.1.0'],
       cachePath: './test/.test-cache',
       fhirVersion: '4.0.1',
       cacheMode: 'lazy'
